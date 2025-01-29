d40c438790b7c3ea2b32b26e14dfabc5d91052b3
MaryVienaLouiseCTongcua <maryvienalouise.tongcua@smc.pshs.edu.ph>
2025-01-29 20:58:21 +0800

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,11 @@
+let nickname = "Mary";
+let heightInches = 62;
+let weightKg = 40;
+
+
+	let heightFeet = Math.floor(heightInches / 12);
+	let remainingInches = heightInches % 12;
+	let weightLbs = weightKg * 2.20462;
+
+
+alert(`Name: ${nickname}\nHeight: ${heightFeet}'${remainingInches}"\nWeight: ${weightLbs.toFixed(3)} lbs`);
